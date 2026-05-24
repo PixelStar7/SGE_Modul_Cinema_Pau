00946888e66ff6866c7f06041b3601dbbd03d6cf
Pau Tort Balsach <pautortbalsach@gmail.com>
2026-05-24 15:04:13 +0200

    Primer Commit, Esquelet creat

---
+++ __manifest__.py
@@ -0,0 +1,23 @@
+{ 
+    'name': 'cinema_pau', 
+    'version': '1.0', 
+    'category': 'Education', 
+    'summary': 'Educational module for Odoo practice', 
+        Module prepared by department 'Informatica i comunicacions' 
+        of Institute Mila i Fontanals in Igualada (Barcelona-Spain) 
+        for learning in development and adaptation of modules of Odoo ERP. 
+ 
+        It is part of the learning materials for the module 
+        'Sistemes de gestio empresarial' in the course 
+        'CFS Desenvolupament d'aplicacions multiplataforma'. 
+    'author': 'Pau Tort Balsach', 
+    'website': 'http://www.infomila.info', 
+    'license': 'LGPL-3', 
+    'depends': ['base'], 
+    'data': [ 
+        'security/ir.model.access.csv', 
+        'views/views.xml', 
+    ], 
+    'installable': True, 
+    'application': True, 
+} 
