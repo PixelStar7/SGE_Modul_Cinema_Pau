28e5250e2eba4458c50dfa38ae48fdd04f920c2c
Pau Tort Balsach <pautortbalsach@gmail.com>
2026-05-24 20:11:54 +0200

    Comença l'examen

--- __manifest__.py
+++ __manifest__.py
@@ -1,23 +1,28 @@
-{ 
-    'name': 'cinema_pau', 
-    'version': '1.0', 
-    'category': 'Education', 
-    'summary': 'Educational module for Odoo practice', 
-        Module prepared by department 'Informatica i comunicacions' 
-        of Institute Mila i Fontanals in Igualada (Barcelona-Spain) 
-        for learning in development and adaptation of modules of Odoo ERP. 
- 
-        It is part of the learning materials for the module 
-        'Sistemes de gestio empresarial' in the course 
-        'CFS Desenvolupament d'aplicacions multiplataforma'. 
-    'author': 'Pau Tort Balsach', 
-    'website': 'http://www.infomila.info', 
-    'license': 'LGPL-3', 
-    'depends': ['base'], 
-    'data': [ 
-        'security/ir.model.access.csv', 
-        'views/views.xml', 
-    ], 
-    'installable': True, 
-    'application': True, 
-} 
+# -*- coding: utf-8 -*-
+{
+    'name': 'Cinema',
+    'version': '1.0',
+    'category': 'Entertainment',
+    'summary': 'Cinema Management',
+    'description': """
+          Module prepared by department 'Informàtica i comunicacions'
+          of Institute Milà i Fontanals in Igualada (Barcelona-Spain)
+          for learning in development and adaptation of modules of Odoo ERP.
+
+          It is part of the learning materials for the module
+          'Sistemes de gestió empresarial' in the course
+          'CFS Desenvolupament d''aplicacions multiplataforma'.
+    """,
+    'author': 'Group DAM2 - Course 2025-2026',
+    'website': 'http://www.infomila.info',
+    'depends': ['base', 'board', 'mail'],
+    'data': [
+        # 'security/cinema_security.xml', # Primer la definció dels grups de seguretat 
+        # 'security/ir.model.access.csv', # Després les regles d'accés als models
+        'views/cinema_views.xml'
+    ],
+    'demo': [],
+    'installable': True,
+    'application': True,
+    'license': 'LGPL-3',
+}
